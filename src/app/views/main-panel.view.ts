94a5545a04cb1b1de8704a08dd01f85227598b02
greinisch <greinisch@youwol.com>
2021-12-15 18:26:49 +0100

    :bug: main-panel=> use custom Tabs to keep tab in memory (but hidden)

--- src/app/views/main-panel.view.ts
+++ src/app/views/main-panel.view.ts
@@ -1,5 +1,4 @@
-import { VirtualDOM } from "@youwol/flux-view";
-import { Tabs } from "@youwol/fv-tabs";
+import { attr$, children$, childrenWithReplace$, VirtualDOM } from "@youwol/flux-view";
 
 import { map } from "rxjs/operators";
 import { AppState } from "../app-state";
@@ -8,10 +7,14 @@ import { DashboardView } from "./dashboard/dashboard.view";
 import { ProjectView } from "./project/project.view";
 
 
-export class DashboardTab extends Tabs.TabData {
+export class DashboardTab {
 
-    constructor() {
-        super('dashboard', 'Dashboard')
+    public readonly id = 'dashboard'
+
+    constructor() { }
+
+    contentView(state: AppState) {
+        return new DashboardView({ state })
     }
 
     headerView(state: AppState): VirtualDOM {
@@ -20,18 +23,19 @@ export class DashboardTab extends Tabs.TabData {
             class: 'p-1 rounded border'
         }
     }
-
-    view(state: AppState): VirtualDOM {
-
-        return new DashboardView({ state })
-    }
 }
 
-export class ProjectTab extends Tabs.TabData {
 
+export class ProjectTab {
+
+    public readonly id: string
 
     constructor(public readonly project: Project) {
-        super(project.id, project.name)
+        this.id = project.id
+    }
+
+    contentView(state: AppState) {
+        return new ProjectView({ state, project: this.project })
     }
 
     headerView(state: AppState): VirtualDOM {
@@ -39,28 +43,24 @@ export class ProjectTab extends Tabs.TabData {
             class: ' d-flex align-items-center p-1 rounded border',
             children: [
                 {
-                    innerText: this.name,
+                    innerText: `${this.project.name}#${this.project.version}`,
                 },
                 {
                     class: 'fas fa-times fv-text-error fv-xx-darker fv-hover-xx-lighter pl-2 mx-2',
                     onclick: (ev) => {
                         ev.stopPropagation()
-                        state.closeProject(this.id)
+                        state.closeProject(this.project.id)
                     }
                 }
             ]
         }
     }
-
-    view(state: AppState): VirtualDOM {
-
-        return new ProjectView({ state, project: this.project })
-    }
 }
+
 
 export class MainPanelView implements VirtualDOM {
 
-    public readonly class = "w-100 h-100 p-3"
+    public readonly class = "w-100 h-100 p-3 d-flex flex-column"
     public readonly state: AppState
 
     public readonly children: VirtualDOM[]
@@ -72,21 +72,52 @@ export class MainPanelView implements VirtualDOM {
         let tabsData$ = this.state.openProjects$.pipe(
             map(projects => [new DashboardTab(), ...projects.map(p => new ProjectTab(p))])
         )
-
-        let tabState = new Tabs.State(tabsData$, this.state.selectedTabId$)
-
+        let wrapChild$ = (targetId, view) => (
+            {
+                class: attr$(
+                    this.state.selectedTabId$,
+                    (id) => id == targetId ? 'h-100' : 'd-none'
+                ),
+                children: [view]
+            }
+        )
         this.children = [
-            new Tabs.View({
-                state: tabState,
-                contentView: (_, tabData: DashboardTab | ProjectTab) => {
-                    return tabData.view(this.state)
-                },
-                headerView: (_, tabData: DashboardTab | ProjectTab) => {
-                    return tabData.headerView(this.state)
-                },
-                class: 'h-100 d-flex flex-column'
-            } as any
-            )
+            new HeaderView({ state: this.state, tabsData$ }),
+            {
+                class: 'flex-grow-1 border',
+                style: { minHeight: '0px' },
+                children: childrenWithReplace$(
+                    tabsData$,
+                    (tab: DashboardTab | ProjectTab) => wrapChild$(tab.id, tab.contentView(this.state)),
+                    { comparisonOperator: (lhs, rhs) => lhs.id == rhs.id }
+                )
+            }
         ]
     }
+}
+
+
+export class HeaderView implements VirtualDOM {
+
+    public readonly class = "w-100 d-flex align-items-center fv-pointer"
+    public readonly children: any
+
+    constructor(params: { state: AppState, tabsData$ }) {
+
+        this.children = children$(
+            params.tabsData$,
+            (tabs) => tabs.map(tab => ({
+                class: attr$(
+                    params.state.selectedTabId$,
+                    (id) => id == tab.id ? 'fv-text-focus' : '',
+                    { wrapper: (d) => `${d} fv-hover-bg-background-alt` }
+                ),
+                children: [
+                    tab.headerView(params.state)
+                ],
+                onclick: () => params.state.selectTab(tab.id)
+            }))
+        )
+    }
+
 }
