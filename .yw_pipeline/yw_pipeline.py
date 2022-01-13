bdb2fcf30d556a2e352c2ab57fb560ebc98e36c2
greinisch <greinisch@youwol.com>
2022-01-13 18:34:37 +0100

    :alien: :wrench: sync yw_pipeline with new py-youwol

--- .yw_pipeline/yw_pipeline.py
+++ .yw_pipeline/yw_pipeline.py
@@ -1,5 +1,5 @@
 import youwol.pipelines.pipeline_typescript_weback_npm
-from youwol.configuration.models_config import IPipelineFactory
+from youwol.environment.models import IPipelineFactory
 
 
 class PipelineFactory(IPipelineFactory):
