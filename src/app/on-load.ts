933c22ff0ada794f2ed854762d6a75b8233890a5
greinisch <greinisch@youwol.com>
2022-04-28 07:19:31 +0200

    🐛 on-load => wait for ws connection before displaying app

--- src/app/on-load.ts
+++ src/app/on-load.ts
@@ -1,6 +1,14 @@
 import { render } from '@youwol/flux-view'
 import { AppView } from './app-view'
+import { PyYouwol } from '@youwol/http-clients'
+import { take } from 'rxjs/operators'
+
 require('./style.css')
 
-const vDOM = new AppView()
-document.body.appendChild(render(vDOM))
+new PyYouwol.PyYouwolClient()
+    .webSocket$()
+    .pipe(take(1))
+    .subscribe(() => {
+        const vDOM = new AppView()
+        document.body.appendChild(render(vDOM))
+    })
